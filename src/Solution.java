1678e6fa4e5c743c88f2ffb25b34505f96998296
LebedevMikhail <lebedev1911@yandex.ru>
2018-06-30 14:31:26 +0300

    Solved problem 728

--- src/Solution.java
+++ src/Solution.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
 
     //771. Jewels and Stones
@@ -37,6 +40,26 @@ public class Solution {
         }
         boolean check = false;
         if ((position[0][0] == position[1][0]) && (position[0][1] == position[1][1])) check = true;
+        return check;
+    }
+
+    //728. Self Dividing Numbers
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = left; i <= right; i++) {
+            if (isSelfDivine(i)) numbers.add(i);
+        }
+        return numbers;
+    }
+
+    private static boolean isSelfDivine(int i) {
+        boolean check = true;
+        int number = i;
+        while (number > 0) {
+            int remainder = number % 10;
+            number /= 10;
+            if ((remainder == 0) || ((i % remainder) > 0)) check = false;
+        }
         return check;
     }
 }
