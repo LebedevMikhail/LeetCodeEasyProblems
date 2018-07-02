0d83825da8a6c639a0577a5e92eddce91e67c851
LebedevMikhail <lebedev1911@yandex.ru>
2018-07-02 22:40:54 +0300

    correct mistakes

--- src/judgeRouteCircle/Solution.java
+++ src/judgeRouteCircle/Solution.java
@@ -3,6 +3,7 @@ package judgeRouteCircle;
 public class Solution {
 
     //657. Judge Route Circle
+
     // Initially, there is a Robot at position (0, 0). Given a sequence of its moves, judge if this robot makes a circle, which means it moves back to the original place.
 
     //The move sequence is represented by a string.
--- src/reverseWordsStringIII/Solution.java
+++ src/reverseWordsStringIII/Solution.java
@@ -1,4 +1,4 @@
-package reverseWordsinaStringIII;
+package reverseWordsStringIII;
 
 public class Solution {
 
